b51fd0f41fb75032a3c862ef26c976568da96dbf
mary-rose139 <chinyeren38@gmail.com>
2025-09-12 15:57:38 -0700

    first upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll(".btn");
+const submitBtn = document.querySelector(".submit");
+const ratingText = document.getElementById("selected-rating");
+
+let rating = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((b) => {
+      b.classList.remove("active");
+    });
+    button.classList.add("active");
+    let value = button.textContent;
+    rating = value;
+  });
+});
+
+submitBtn.addEventListener("click", () => {
+  if (rating > 0) {
+    ratingText.textContent = rating;
+
+    setTimeout(() => {
+      const ratingCard = document.querySelector(".container");
+      const thankCard = document.querySelector(".thank-you-hidden");
+
+      ratingCard.style.display = "none";
+      thankCard.style.display = "block";
+    }, 2000);
+  } else {
+    alert("Please pick one rating!");
+  }
+});
